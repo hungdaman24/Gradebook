d66e62b7897e7b78a3cffc3deef9f17e4bc3f7ef
hphuyn3d <hung.huynh@payfactors.com>
2020-04-28 00:33:21 -0400

    Moved folders, created unit tests

---
+++ src/models/Statistics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gradebook.models
+{
+    public class Statistics
+    {
+        public double Average;
+        public double High;
+        public double Low;
+    }
+}
